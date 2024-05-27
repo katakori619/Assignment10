1a7490bb9db493c723802a4a1a29c8e3fea75e13
katakori619 <kmdht9012@gmail.com>
2024-05-27 23:00:47 +0300

    amogos

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,8 @@
 let bookName = document.getElementById('bookname')
 let url = document.getElementById('URL')
+let contentId = document.getElementById('content')
 let arr
-
+let urlvalid , booknamevalid
 if(localStorage.getItem('books') == null){
     arr = []
 }
@@ -15,11 +16,16 @@ function add(){
         id: bookName.value ,
         link: url.value
     }
-    arr.push(book)
-    display()
-    localStorage.setItem('books',JSON.stringify(arr))
-    console.log(arr)
-    clear()
+    if(urlvalid && booknamevalid){
+        arr.push(book)
+        display()
+        localStorage.setItem('books',JSON.stringify(arr))
+        console.log(arr)
+        clear()
+    }
+    else{
+        contentId.classList.remove('d-none')
+    }
 }
 
 function display(){
@@ -54,4 +60,40 @@ function remove(index){
 function clear(){
     bookName.value = null
     url.value = null
+    bookName.classList.remove('is-valid')
+    bookName.classList.remove('is-invalid')
+    url.classList.remove('is-valid')
+    url.classList.remove('is-valid')
+}
+
+function validname(element){
+    var valid = /^[A-Z][a-z0-9 ?]{2,14}$/
+    if(valid.test(element.value)){
+        element.classList.add('is-valid')
+        element.classList.remove('is-invalid')
+        booknamevalid = true
+    }
+    else{
+        element.classList.add('is-invalid')
+        element.classList.remove('is-valid')
+        booknamevalid = false
+    }
+}
+
+function validurl(element){
+    var valid = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/
+    if(valid.test(element.value)){
+        element.classList.add('is-valid')
+        element.classList.remove('is-invalid')
+        urlvalid = true
+    }
+    else{
+        element.classList.add('is-invalid')
+        element.classList.remove('is-valid')
+        urlvalid = false
+    }
+}
+
+function Close(){
+    contentId.classList.add('d-none')
 }
